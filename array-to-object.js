fd3172d4c64cff260e88d371a61dd05e5f70d090
igoodbad <totocod2@gmail.com>
2018-12-28 15:16:36 -0600

    Remove variable declaration in file .js

--- array-to-object.js
+++ array-to-object.js
@@ -1,9 +1,3 @@
-let dataArray = [
-    { letras: ["a", "b", "d"] },
-    { booleano: false },
-    { objeto: { otroNumero: 1, unaPalabra: "arandano", otroBoleano: false } },
-    { arrayNuevo: ["otras", "cosas", 1, 2, false, true] }
-];
 function arrayToObject() {
     let newObject = {};
     for(let value of dataArray){
