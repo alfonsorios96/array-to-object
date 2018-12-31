1b2113b9b44ef2d4124d743ef0d42116cb194aa5
Alfonso Rios <malforime@gmail.com>
2018-12-31 15:25:15 -0600

    Refactor by reduce

--- array-to-object.js
+++ array-to-object.js
@@ -1,8 +1,5 @@
-function arrayToObject(dataArray) {
-    let newObject = {};
-    for(let value of dataArray){
-        let separed = Object.entries(value)[0];
-        newObject[separed[0]] = separed[1];
-    }
-    return newObject;
-}
+const arrayToObject = dataArray => dataArray.reduce((accumulator, item) => {
+    const separed = Object.entries(item)[0];
+    accumulator[separed[0]] = separed[1];
+    return accumulator;
+});
